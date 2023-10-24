a7cd1ae99e86519e463db2dd1e3496d101d73ba3
standard-robot <jonahjperry5@gmail.com>
2023-10-24 01:54:51 -0500

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://standardrobot.dev',
   integrations: [
+    sitemap(),
     tailwind(), 
     react({
       experimentalReactChildren: true,
