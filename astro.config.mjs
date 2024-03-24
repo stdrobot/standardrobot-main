3ae784a0074b1baabbc254919211cfec6d36e3df
standard-robot <jonahjperry5@gmail.com>
2024-03-24 12:57:43 -0500

    blog?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,24 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from "@astrojs/sitemap";
-import react from "@astrojs/react";
+import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
+
+import sanity from '@sanity/astro';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://standardrobot.dev',
   integrations: [
     sitemap(),
-    tailwind(), 
+    tailwind(),
     react({
       experimentalReactChildren: true,
-    })
-  ]
+    }),
+    sanity({
+      projectId: 'yvcfgs0v',
+      dataset: 'production',
+      useCdn: false,
+      studioBasePath: '/admin',
+    }),
+  ],
 });
